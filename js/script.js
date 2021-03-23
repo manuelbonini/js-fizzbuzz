19cd0af332be5415c99566dab9c8349bf5abf4fb
Manuel Bonini <manuelbonini17@gmail.com>
2021-03-23 16:10:30 +0100

    modifiche

--- js/script.js
+++ js/script.js
@@ -1,15 +1,12 @@
-// variabili
-var numero= 1;
 
 for( i = 0 ; i < 100 ; i++) {
-    if(numero % 3 == 0 && numero % 5 == 0) {
+    if((i + 1) % 3 == 0 && (i + 1) % 5 == 0) {
         console.log('FizzBuzz');
-    }else if(numero % 3 == 0) {
+    }else if((i + 1) % 3 == 0) {
         console.log('Fizz');
-    }else if(numero % 5 == 0) {
+    }else if((i + 1) % 5 == 0) {
         console.log('Buzz');
     }else {
-        console.log(numero);
+        console.log((i + 1));
     }
-    numero++;
 }
